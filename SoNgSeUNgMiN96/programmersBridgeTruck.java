860475b6ebffec61501a9699790a5ddee74683e5
SoNgSeUNgMiN96 <87978544+SoNgSeUNgMiN96@users.noreply.github.com>
2021-11-01 13:24:04 +0900

    오타 및 띄어쓰기 수정

--- SoNgSeUNgMiN96/programmersBridgeTruck.java
+++ SoNgSeUNgMiN96/programmersBridgeTruck.java
@@ -22,17 +22,17 @@ class Solution {
 
         for (int i=0;i<truck_weights.length;i++){   //모든 트럭을 순서대로
 
-            if(queue.size()>0&&time ==queue.peek()[1]){     //현재 시간 기준으로 나올 트럭이 있다면,
+            if(queue.size()>0&&time ==queue.peek()[1]){     //현재 시간 기준으로 나올 트럭이 있다면
                 sum -=queue.poll()[0];
             }
-            if(sum+truck_weights[i]<=weight){       //트럭이 추가로 올라 탈 수 있다면
+            if(sum+truck_weights[i]<=weight){       //트럭이 추가로 올라탈 수 있다면
                 sum +=truck_weights[i];     //총 무게를 더해주고
                 Integer temp[] = new Integer[2];
                 temp[0]=truck_weights[i];       //트럭의 무게와 트럭이 나올 시간을 큐에 넣어준다
                 temp[1] = time+bridge_length;
                 queue.add(temp);
                 time++;     //1초 진행
-            }else{      //이미 무게 초과인 상태라면 다음 트럭이 올라갈 수 있는 시간으로 이동하고, 동일트럭을 재검사한다.
+            }else{      //이미 무게 초과인 상태라면 다음 트럭이 올라갈 수 있는 시간으로 이동하고, 동일 트럭을 재검사한다.
                 time = queue.peek()[1];
                 i--;
             }
